6bcd93eb869ec2bad8b227562bc10a1eb07663fb
Nguyen Ho Quoc Thinh <thinhnhq@Nguyens-Mac-mini.local>
2024-04-08 23:26:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Day1 from "./days/1";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Day1 />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/days/1/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Day1 = () => {
+  return <div className="container"></div>;
+};
+
+export default Day1;
