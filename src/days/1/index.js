4b1c89af55dbe39a2b2dfe6bcefc99b20a3e5980
Nguyen Ho Quoc Thinh <thinhnhq@Nguyens-Mac-mini.local>
2024-04-09 00:15:21 +0700

    day-1: complete day 1 css challenge

--- src/days/1/index.js
+++ src/days/1/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./styles.scss";
 
+const One = () => {
+  return (
+    <div className="number-one">
+      <div className="number-one-first-part" />
+      <div className="number-one-second-part" />
+    </div>
+  );
+};
+
+const Zero = () => <div className="number-zero" />;
+
 const Day1 = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="number">
+        <div className="one">
+          <One />
+        </div>
+        <div className="first-zero">
+          <Zero />
+        </div>
+        <div className="second-zero">
+          <Zero />
+        </div>
+      </div>
+      <span className="days">DAYS</span>
+      <span className="challenge">CSS CHALLENGE</span>
+    </div>
+  );
 };
 
 export default Day1;
