bdacf46b5ad2cf99b707d2b4a5b124533987ae83
thinhnhq <45492918+thinhnhq@users.noreply.github.com>
2024-04-19 16:44:53 +0700

    day-4 complete day 4 css challenge (#4)
    Co-authored-by: Nguyen Ho Quoc Thinh <thinhnhq@Nguyens-Mac-mini.local>

---
+++ src/days/4/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.scss";
+
+const Day4 = () => {
+  return (
+    <div className="container">
+      <div className="circle-1" />
+      <div className="circle-2" />
+      <div className="circle-3" />
+    </div>
+  );
+};
+
+export default Day4;
