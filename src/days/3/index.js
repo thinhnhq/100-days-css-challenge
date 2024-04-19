cc2eceadcc3698aea2d3da5aa4b56d701c4bb9cb
thinhnhq <45492918+thinhnhq@users.noreply.github.com>
2024-04-19 15:54:26 +0700

    day-3 complete day 3 css challenge (#3)
    Co-authored-by: Nguyen Ho Quoc Thinh <thinhnhq@Nguyens-Mac-mini.local>

---
+++ src/days/3/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.scss";
+
+const Day3 = () => {
+  return (
+    <div className="container">
+      <div className="circle">
+        <div className="sky" />
+        <div className="sun" />
+        <div className="pyramid-front-face" />
+        <div className="pyramid-side-face" />
+        <div className="shadow" />
+        <div className="ground" />
+      </div>
+    </div>
+  );
+};
+
+export default Day3;
